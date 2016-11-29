dc3e37fb59c3e9be623bd67c5d2e7ee3c5746057
Anastasia Taing <anastasiassd@gmail.com>
2016-11-29 01:17:28 -0500

    finish key generator

--- Crypto HW8/src/RSA.java
+++ Crypto HW8/src/RSA.java
@@ -3,27 +3,37 @@ import java.security.SecureRandom;
 
 
 public class RSA {
-
-	public static void main(String args[]) {
-		generateKey();
-	}
 	
-	public static void generateKey() {
-		BigInteger p = BigInteger.valueOf(17);
-		BigInteger q = BigInteger.valueOf(11);
+	private BigInteger[] privateKey;
+	public BigInteger[] publicKey;
+	
+	public void generateKey() {
+		BigInteger p = BigInteger.probablePrime(1024, new SecureRandom());
+		BigInteger q = BigInteger.probablePrime(1024, new SecureRandom());
 		BigInteger temp1 = p.subtract(BigInteger.ONE);
 		BigInteger temp2 = q.subtract(BigInteger.ONE);
 		BigInteger fn = temp1.multiply(temp2);
-		System.out.println(fn.toString() + " " + findE(fn));
+		BigInteger e = findE(fn);
+		BigInteger d = e.modInverse(fn);
+		
+		BigInteger pq = p.multiply(q);
+		publicKey = new BigInteger[2];
+		publicKey[0] = e;
+		publicKey[1] = pq;
+		privateKey = new BigInteger[2];
+		privateKey[0] = d;
+		privateKey[1] = pq;
+
 	}
 	
-	private static BigInteger findE(BigInteger fn) {
+	private BigInteger findE(BigInteger fn) {
 
 		while (true) {
 			BigInteger e = new BigInteger(1023, new SecureRandom());
 			if (fn.gcd(e).equals(BigInteger.ONE)) return e;
 		}
 	}
+	
 	
 
 }
---
+++ Crypto HW8/src/Tester.java
@@ -0,0 +1,11 @@
+
+public class Tester {
+
+	public static void main(String[] args) {
+		//does not do anything rn, just checks if RSA compiles
+		RSA test = new RSA();
+		test.generateKey();
+
+	}
+
+}
