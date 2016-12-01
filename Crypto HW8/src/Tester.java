e6e8c605d6460a861175f75d44db810eb0c995c3
nguyenh8 <nguyenh8@G108LAB-32.ac.stockton>
2016-12-01 11:35:27 -0500

    Included public key and signed message in code

--- Crypto HW8/src/Tester.java
+++ Crypto HW8/src/Tester.java
@@ -7,21 +7,35 @@ public class Tester {
 		RSA test = new RSA();
 		test.generateKey();
 
+		//decrypt question
 		BigInteger M = RSAHelper.C4.modPow(RSAHelper.eVince, RSAHelper.nVince);
 		
 		String X = RSAHelper.recoverTextFromBigInteger(M);
-		
 		System.out.println(X);
 		
 		//encode string answer into BigInteger
 		BigInteger encodedAnswer = RSAHelper.encodeMessageAsBigInteger("Rivest, Shamir and Adleman.");
 		
-		//Uhh, am I doing this right?
 		//"signed" our answer by encrypting with our private key
 		BigInteger signedAnswer = encodedAnswer.modPow(RSA.privateKey[0],RSA.privateKey[1]);
 		
 		//Encrypt our encrypted answer with his public key
 		BigInteger signedEncryptedAnswer = signedAnswer.modPow(RSAHelper.eVince, RSAHelper.nVince);
+		
+		//encrypted message in decimal
+		//System.out.println(signedEncryptedAnswer);
+		
+		//e
+		//System.out.println(RSA.publicKey[0].toString());
+		
+		//n
+		//System.out.println(RSA.publicKey[1].toString());
+		
+		//decrypt our answer
+		BigInteger decryptedAnswer = signedAnswer.modPow(RSA.publicKey[0],RSA.publicKey[1]);
+		String H = RSAHelper.recoverTextFromBigInteger(decryptedAnswer);
+		System.out.print("Answer: " + H);
+		
 	}
 
 }
